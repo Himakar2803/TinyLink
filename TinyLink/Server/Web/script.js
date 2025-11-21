00c69af45809883268c45b5669b52f47bf383ccd
Himakar2803 <himakarsiddabathula@gmail.com>
2025-11-21 21:31:13 +0530

    Create script.js

---
+++ TinyLink/Server/Web/script.js
@@ -0,0 +1,23 @@
+const API = "https://tinylink-1-b6zq.onrender.com";
+
+// Create Short Link
+function createLink() {
+  const target = document.getElementById("targetUrl").value;
+  const code = document.getElementById("customCode").value;
+
+  fetch(`${API}/api/links`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ target, code })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.error) {
+      document.getElementById("msg").innerText = "❌ " + data.error;
+    } else {
+      document.getElementById("msg").innerHTML =
+        "✅ Short Link Created: " +
+        `<a href="${API}/${data.code}" target="_blank">${API}/${data.code}</a>`;
+    }
+  });
+}
